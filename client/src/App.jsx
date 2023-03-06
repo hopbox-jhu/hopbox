f8c7ee13aff6b3bdbb15d6a92059ffef0ff2f1cb
KiwiThePoodle <42877043+KiwiThePoodle@users.noreply.github.com>
2023-03-06 10:40:31 -0500

    added interactive map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
+import Map from "./components/Map";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="/map" element={<Map />} />
     </Routes>
   );
 }
