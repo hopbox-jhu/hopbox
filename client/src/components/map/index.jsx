e712ca67818b10d3dad7b3f25620d1620baed5ff
KiwiThePoodle <42877043+KiwiThePoodle@users.noreply.github.com>
2023-03-13 10:35:47 -0400

    hard-coded a point at JHU

--- client/src/components/map/index.jsx
+++ client/src/components/map/index.jsx
@@ -20,7 +20,58 @@ function Map() {
             center: [lng, lat],
             zoom: zoom
         });
-    });
+        
+        map.current.on('load', () => {
+            // Add an image to use as a custom marker
+            map.current.loadImage(
+                'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+                (error, image) => {
+                    if (error) throw error;
+                    map.current.addImage('custom-marker', image);
+                    // Add a GeoJSON source with 2 points
+                    map.current.addSource('points', {
+                        'type': 'geojson',
+                        'data': {
+                            'type': 'FeatureCollection',
+                            'features': [
+                                {
+                                    // feature for JHU
+                                    'type': 'Feature',
+                                    'geometry': {
+                                        'type': 'Point',
+                                        'coordinates': [
+                                            lng, lat
+                                        ]
+                                    },
+                                    'properties': {
+                                        
+                                    }
+                                }
+                            ]
+                        }
+                    });
+    
+                    // Add a symbol layer
+                    map.current.addLayer({
+                        'id': 'points',
+                        'type': 'symbol',
+                        'source': 'points',
+                        'layout': {
+                            'icon-image': 'custom-marker',
+                            // get the title name from the source's "title" property
+                            'text-field': ['get', 'title'],
+                            'text-font': [
+                                'Open Sans Semibold',
+                                'Arial Unicode MS Bold'
+                            ],
+                            'text-offset': [0, 1.25],
+                            'text-anchor': 'top'
+                        }
+                    });
+                }
+            );
+        });
+      });
 
     useEffect(() => {
         if (!map.current) return; // wait for map to initialize
