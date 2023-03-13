4e1d7a59fe4df8b875c1ddda6c0b2e9d8bcef932
Thomas-Yu <42897266+Thomas-Yu@users.noreply.github.com>
2023-03-13 13:10:37 -0400

    some styling

--- client/src/components/SigninSection/SignInPage.jsx
+++ client/src/components/SigninSection/SignInPage.jsx
@@ -10,18 +10,18 @@ function SignInPage() {
   }
 
   return (
-    <div>
-      <h2>Sign In</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', backgroundColor: '#eae9f2' }}>
+      <h2 style={{ marginBottom: '2rem', color: '#d6008e' }}>Sign In</h2>
+      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: 'white', padding: '2rem', borderRadius: '0.5rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)' }}>
+        <label style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1rem' }}>
           Email:
-          <input type="email" value={email} onChange={event => setEmail(event.target.value)} />
+          <input type="email" value={email} onChange={event => setEmail(event.target.value)} style={{ marginTop: '0.5rem', padding: '0.5rem', borderRadius: '0.25rem', border: '1px solid #d6008e' }} />
         </label>
-        <label>
+        <label style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1rem' }}>
           Password:
-          <input type="password" value={password} onChange={event => setPassword(event.target.value)} />
+          <input type="password" value={password} onChange={event => setPassword(event.target.value)} style={{ marginTop: '0.5rem', padding: '0.5rem', borderRadius: '0.25rem', border: '1px solid #d6008e' }} />
         </label>
-        <button type="submit">Sign In</button>
+        <button type="submit" style={{ marginTop: '1rem', padding: '0.5rem 1rem', backgroundColor: '#d6008e', color: 'white', borderRadius: '0.25rem', border: 'none', cursor: 'pointer' }}>Sign In</button>
       </form>
     </div>
   );
--- client/src/components/SignupSection/SignUpPage.jsx
+++ client/src/components/SignupSection/SignUpPage.jsx
@@ -17,31 +17,34 @@ function SignUpPage() {
   };
 
   return (
-    <div>
-        <h2>Sign Up</h2>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name</label>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', backgroundColor: '#eae9f2' }}>
+        <h2 style={{ marginBottom: '2rem', color: '#d6008e' }}>Sign Up</h2>
+        <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: 'white', padding: '2rem', borderRadius: '0.5rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)' }}>
+            <label htmlFor="name" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1rem' }}>Name</label>
             <input
             id="name"
             type="text"
             value={name}
             onChange={(event) => setName(event.target.value)}
+            style={{ marginTop: '0.5rem', padding: '0.5rem', borderRadius: '0.25rem', border: '1px solid #d6008e' }}
             />
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1rem' }}>Email</label>
             <input
             id="email"
             type="email"
             value={email}
             onChange={(event) => setEmail(event.target.value)}
+            style={{ marginTop: '0.5rem', padding: '0.5rem', borderRadius: '0.25rem', border: '1px solid #d6008e' }}
             />
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1rem' }}>Password</label>
             <input
             id="password"
             type="password"
             value={password}
             onChange={(event) => setPassword(event.target.value)}
+            style={{ marginTop: '0.5rem', padding: '0.5rem', borderRadius: '0.25rem', border: '1px solid #d6008e' }}
             />
-            <button type="submit">Sign up</button>
+            <button type="submit" style={{ marginTop: '1rem', padding: '0.5rem 1rem', backgroundColor: '#d6008e', color: 'white', borderRadius: '0.25rem', border: 'none', cursor: 'pointer' }}>Sign up</button>
         </form>
     </div>
   );
