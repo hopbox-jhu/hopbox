b744476d18ccba539da1ae490edcc809264f14f7
ye1esther <yshin31@jhu.edu>
2023-04-03 01:16:01 -0400

    Sign In and Sign Up Page UI Upgraded

--- client/src/components/SigninSection/SignInPage.jsx
+++ client/src/components/SigninSection/SignInPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Container, Title, Form, Label, Input, Button, LinkText, LinkStyled, IconInput } from './SignInElements';
 
 function SignInPage() {
   const [email, setEmail] = useState('');
@@ -17,21 +17,19 @@ function SignInPage() {
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', backgroundColor: '#eae9f2' }}>
-      <h2 style={{ marginBottom: '2rem', color: '#d6008e' }}>Sign In</h2>
-      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: 'white', padding: '2rem', borderRadius: '0.5rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)' }}>
-        <label style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1rem' }}>
-          Email:
-          <input type="email" value={email} onChange={event => setEmail(event.target.value)} style={{ marginTop: '0.5rem', padding: '0.5rem', borderRadius: '0.25rem', border: '1px solid #d6008e' }} />
-        </label>
-        <label style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1rem' }}>
-          Password:
-          <input type="password" value={password} onChange={event => setPassword(event.target.value)} style={{ marginTop: '0.5rem', padding: '0.5rem', borderRadius: '0.25rem', border: '1px solid #d6008e' }} />
-        </label>
-        <button type="submit" style={{ marginTop: '1rem', padding: '0.5rem 1rem', backgroundColor: '#d6008e', color: 'white', borderRadius: '0.25rem', border: 'none', cursor: 'pointer' }}>Sign In</button>
-      </form>
-      <p style={{ marginTop: '1rem' }}>Don't have an account? <Link to="/signup" style={{ color: '#d6008e', textDecoration: 'underline' }}>Create one</Link>.</p>
-    </div>
+    <Container>
+    <img src="/src/assets/logo.png" alt="Logo" style={{ height: '120px' , padding: '20px'}} />
+      <Form onSubmit={handleSubmit}>
+        <Label>
+          <IconInput type="email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Email" icon="email" />
+        </Label>
+        <Label>
+        <IconInput type="password" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="Password" icon="password"/>
+        </Label>
+        <Button type="submit" style={{ height: '40px' }}>Sign In</Button>
+      </Form>
+      <LinkText>Don't have an account? <LinkStyled to="/signup">Create one</LinkStyled>.</LinkText>
+    </Container>
   );
 }
 
--- client/src/components/SignupSection/SignUpPage.jsx
+++ client/src/components/SignupSection/SignUpPage.jsx
@@ -1,52 +1,38 @@
-import React, { useState } from "react";
-
+import React, { useState } from 'react';
+import { Container, Title, Form, Label, Input, Button, LinkText, LinkStyled, IconInput } from './SignUpElements';
 
 function SignUpPage() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleSubmit = async (event) => {
+  async function handleSubmit(event) {
     event.preventDefault();
     try {
       await createUser({ name, email, password }); // Create user document in MongoDB
-      alert("Successfully signed up!");
+      alert('Successfully signed up!');
     } catch (error) {
-      alert("Error signing up");
+      alert('Error signing up');
     }
-  };
+  }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', backgroundColor: '#eae9f2' }}>
-      <h2 style={{ marginBottom: '2rem', color: '#d6008e' }}>Sign Up</h2>
-      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: 'white', padding: '2rem', borderRadius: '0.5rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)' }}>
-        <label htmlFor="name" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1rem' }}>Name</label>
-        <input
-          id="name"
-          type="text"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-          style={{ marginTop: '0.5rem', padding: '0.5rem', borderRadius: '0.25rem', border: '1px solid #d6008e', width: '100%' }}
-        />
-        <label htmlFor="email" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1rem' }}>Email</label>
-        <input
-          id="email"
-          type="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          style={{ marginTop: '0.5rem', padding: '0.5rem', borderRadius: '0.25rem', border: '1px solid #d6008e', width: '100%' }}
-        />
-        <label htmlFor="password" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1rem' }}>Password</label>
-        <input
-          id="password"
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          style={{ marginTop: '0.5rem', padding: '0.5rem', borderRadius: '0.25rem', border: '1px solid #d6008e', width: '100%' }}
-        />
-        <button type="submit" style={{ marginTop: '1rem', padding: '0.5rem 1rem', backgroundColor: '#d6008e', color: 'white', borderRadius: '0.25rem', border: 'none', cursor: 'pointer', width: '100%' }}>Sign up</button>
-      </form>
-    </div>
+    <Container>
+    <img src="/src/assets/logo.png" alt="Logo" style={{ height: '120px' , padding: '20px'}} />
+      <Form onSubmit={handleSubmit}>
+        <Label>
+          <IconInput type="text" value={name} onChange={(event) => setName(event.target.value)} placeholder="Name" icon="name" />
+        </Label>
+        <Label>
+          <IconInput type="email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Email" icon="email" />
+        </Label>
+        <Label>
+          <IconInput type="password" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="Password" icon="password"/>
+        </Label>
+        <Button type="submit">Sign Up</Button>
+      </Form>
+      <LinkText>Already have an account? <LinkStyled to="/signin">Sign in</LinkStyled>.</LinkText>
+    </Container>
   );
 }
 
