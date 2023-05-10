d80495cc91a206ecf3cf0404b9a25f7f4961dbb3
Thomas-Yu <tennisthomasyu@gmail.com>
2023-05-09 23:10:32 -0400

    im not even sure what i am trying now

--- server/src/index.js
+++ server/src/index.js
@@ -50,7 +50,7 @@ app.use((err, req, res, next) => {
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 
