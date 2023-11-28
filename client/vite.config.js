8cdf42e478f9e15ad66502a144c91b52fb858dd6
nharleytran <98291123+nharleytran@users.noreply.github.com>
2023-11-28 01:53:31 -0800

    aos fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    rollupOptions: {
+      external: ['aos'],
+    },
   },
 });
